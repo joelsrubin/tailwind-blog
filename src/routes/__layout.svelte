ce0acb8c0183a62da7183ac05f482a7222a47e62
joelsrubin <joelsrubin@gmail.com>
2021-10-03 21:35:21 -0500

    add

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,8 @@
+<script>
+  import "../app.css";
+</script>
+
+<main class="container max-w-xl mx-auto px-4">
+  <slot></slot>
+
+</main>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,47 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module">
+  export const load = async ({fetch}) => {
+    const res = await fetch('/posts.json')
+
+    if(res.ok) {
+      const {posts} = await res.json()
+      return {
+        props: {posts},
+      }
+    }
+  }
+</script>
+
+<script>
+  export let posts
+</script>
+
+<svelte:head>
+<title>Tailwind Test Blog | Welcome</title>
+</svelte:head>
+
+
+<h1 class="text-4xl mb-10 font-extrabold">Welcome to SvelteKit</h1>
+
+{#each posts as {title, slug, excerpt, coverImage, tags}}
+<div class="card text-center shadow-2xl mb-20">
+  <figure class="px-10 pt-10">
+    <img class="rounded-xl" src={coverImage.url} alt={`Cover image for ${title}`}/>
+    </figure>
+    <div class="card-body">
+      <h2 class="title">{title}</h2>
+      <p>{excerpt}</p>
+      <div class="flex justify-center mt-5 space-x-2">
+        {#each tags as tag}
+        <span class="badge badge-primary">{tag}</span>
+        {/each}
+        </div>
+        <div class="justify-center card-actions">
+          <a href={`/posts/${slug}`} class="btn btn-outline btn-primary">
+            Read &rAarr;
+            </a>
+          </div>
+
+      </div>
+  </div>
+{/each}
+
